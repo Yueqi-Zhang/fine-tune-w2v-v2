32414add766a5af25bdccbed517d58e41a0cf623
eric.wtlin <eric.wtlin@gmail.com>
2018-02-28 23:01:11 +0800

    update shell script

--- auto_sequential.sh
+++ auto_sequential.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 PROCESS_FLAG="preprocess.py"
 
+if [ $# -ge 2 ]; then
+index_begin=$1
+index_end=$2
+else
 index_begin=1
 index_end=1592
+fi
 
 ###########AUTOMATION######################
-python preprocess.py data/corpus_${index}.0.txt data/wvect.txt data/word2id.txt data/id2word.txt data/topfrequent.txt data/pair/pair_${index}.pkl
-
+for ((index=$index_begin; index<=$index_end; index ++))
+do
+    echo $index
+    python preprocess.py data/corpus_${index}.0.txt data/wvect.txt data/word2id.txt data/id2word.txt data/topfrequent.txt data/pair/pair_${index}.pkl
+done
