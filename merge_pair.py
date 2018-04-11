976128b76f29768c2e8ae60dce0a5a52771d57c4
Yueqi Zhang <vverar1993@gmail.com>
2018-04-11 20:10:45 +0800

    mergepair

---
+++ merge_pair.py
@@ -0,0 +1,21 @@
+import os
+from utils import load_from_pkl, dump_to_pkl
+
+path = 'data/pair'
+files= os.listdir(path)
+pairs = dict()
+for file in files:
+    if not os.path.isdir(file):
+        pair_file_path = path+"/"+file
+        pair = load_from_pkl(pair_file_path)
+        if len(pairs) == 0:
+            pairs = pair
+        else:
+            for key in pair.keys():
+                if key in pairs:
+                    pairs[key] += pair[key]
+                else:
+                    pairs[key] = pair[key]
+
+output_file_name = 'data/pairs'
+dump_to_pkl(pairs, output_file_name)
