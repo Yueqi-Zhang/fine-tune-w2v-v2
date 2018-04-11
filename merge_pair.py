79cb3a663a2533f5ec03b0aad2cf38187c9daf2d
ericwtlin <eric.wtlin@gmail.com>
2018-04-11 22:31:47 +0800

    Update merge_pair.py

--- merge_pair.py
+++ merge_pair.py
@@ -1,13 +1,16 @@
 import os
 from utils import load_from_pkl, dump_to_pkl
+from tqdm import tqdm
+import logging
 
 path = 'data/pair'
 files= os.listdir(path)
 pairs = dict()
-for file in files:
+for file in tqdm(files):
     if not os.path.isdir(file):
         pair_file_path = path+"/"+file
         pair = load_from_pkl(pair_file_path)
+        logging.info("pair size: %d" % (len(pair)))
         if len(pairs) == 0:
             pairs = pair
         else:
@@ -16,6 +19,7 @@ for file in files:
                     pairs[key] += pair[key]
                 else:
                     pairs[key] = pair[key]
+        logging.info("current total pair size: %d" % (len(pairs)))
 
 output_file_name = 'data/pairs'
 dump_to_pkl(pairs, output_file_name)
